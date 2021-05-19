6ef93f414ee3aefa0039d55c6c7635da588ea973
IrenJones <nor.iren@yandex.ru>
2021-05-18 17:57:24 -0700

    [7] add part of the test

--- src/shared/TopBar.test.tsx
+++ src/shared/TopBar.test.tsx
@@ -2,7 +2,45 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import {TopBar} from './TopBar';
 import {render, screen, fireEvent} from '@testing-library/react';
+// import {localStorageMock} from '../setupTests';
+import {AuthStore} from './auth/auth-store';
 
+const {currentUser, tokens} = {
+  currentUser: {
+    id: '12345',
+    login: 'john',
+    display_name: 'jonnie',
+    type: '',
+    broadcaster_type: '',
+    description: '',
+    profile_image_url: 'image.png',
+    offline_image_url: '',
+    view_count: 28,
+    email: 'john@gmail.com',
+    created_at: '2017-01-31T11:08:25.420124Z',
+  },
+  tokens: {
+    access_token: 'token',
+    expires_in: 14068,
+    id_token: 'tokenid',
+    refresh_token: 'refreshtoken',
+    scope: ['openid', 'user_read'],
+    token_type: 'bearer',
+  },
+};
+
+// jest.mock('./auth/auth-store', function () {
+//   return function () {
+//     return {
+//       AuthStore: function () {
+//         this.currentUser = currentUser;
+//         this.tokens = tokens;
+//       },
+//     };
+//   };
+// });
+
+jest.spyOn(AuthStore.prototype, 'user');
 const links = [
   {text: 'Login', location: '#'},
   {text: 'Home', location: '/'},
@@ -10,7 +48,7 @@ const links = [
 ];
 
 describe('TopBar', () => {
-  it('Check if TopBar have all links', async () => {
+  it('has all links', async () => {
     render(
       <BrowserRouter>
         <TopBar />
@@ -22,9 +60,7 @@ describe('TopBar', () => {
       expect(linkDom).toHaveAttribute('href', link.location);
     });
   });
-});
 
-describe('TopBar', () => {
   it('Should open the window when Login button pushed', async () => {
     render(
       <BrowserRouter>
@@ -43,14 +79,20 @@ describe('TopBar', () => {
       'toolbars=yes,scrollbars=yes,width=500,height=500'
     );
   });
-});
 
-describe('TopBar', () => {
-  it('Should show nickname and avatar when logged in', async () => {
-    render(
+  it.only('Should show nickname and avatar when logged in', async () => {
+    // const credsJson = localStorage.getItem('creds');
+    // const {user, tokens} = JSON.parse(credsJson);
+
+    // expect(user.login).toEqual('john');
+
+    const {container} = render(
       <BrowserRouter>
         <TopBar />
       </BrowserRouter>
     );
+
+    expect(container).toMatchSnapshot();
+    // expect(localStorageMock.getItem).toHaveBeenCalledTimes(1);
   });
 });
